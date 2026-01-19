5f26f54e5cb6093ecbde4c4818f0408cd3fe01f2
Barun Panthi Sharma <barunpanthisharma11@gmail.com>
2026-01-19 18:05:30 +0100

    String

---
+++ src/Strings/Comparison.java
@@ -0,0 +1,12 @@
+package Strings;
+
+public class Comparison {
+    static void main() {
+        String a = "Developer";
+        String b = "Developer";
+
+        System.out.println(a == b);
+        System.out.println(a.equals(b));
+        System.out.println(a.charAt(2));
+    }
+}
