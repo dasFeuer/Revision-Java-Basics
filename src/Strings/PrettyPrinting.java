96b5199f8cc235e847ef4d1f9cef0d3cb975f008
Barun Panthi Sharma <barunpanthisharma11@gmail.com>
2026-01-20 20:41:41 +0100

    String formatting

---
+++ src/Strings/PrettyPrinting.java
@@ -0,0 +1,11 @@
+package Strings;
+
+public class PrettyPrinting {
+    static void main(String[] args) {
+        float a = 243.5231f;
+
+        System.out.printf("Formatted number is %.2f \n", a);
+        System.out.printf("Pie number is %.3f\n", Math.PI);
+        System.out.printf("Hello! I am learning %s and %s as well.", "Java", "Spring Boot");
+    }
+}
